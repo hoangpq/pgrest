5a3b6d1b5db5e24bf423ff9edff5235efa32a347
hoangpq <pquochoang2007@gmail.com>
2021-06-29 16:48:41 +0700

    Add unit test

---
+++ .history/test/PgQuerySpec_20210629164336.hs
@@ -0,0 +1,23 @@
+module PgQuerySpec where
+
+-- import           Test.Hspec
+
+import           Database.HDBC
+import           Database.HDBC.PostgreSQL
+
+loadFixture :: String -> IO ()
+loadFixture name = do
+  sql <- readFile $ "test/fixtures/" ++ name ++ ".sql"
+  conn <- connectPostgreSQL "postgres://postgres:@localhost:5432/dbapi_test"
+  runRaw conn sql
+  commit conn
+
+main :: IO ()
+main = beforeAll (loadFixture "schema") $ do
+  describe "insert" $
+    it "can insert into an empty table" $
+      head [23 ..] `shouldBe` (23 :: Int)
+
+  describe "insert again" $
+    it "is true" $
+      True `shouldBe` True
