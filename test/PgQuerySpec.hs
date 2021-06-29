335cca0edaa2757abeb2bbf10cebed0bf2570b35
hoangpq <pquochoang2007@gmail.com>
2021-06-29 20:51:43 +0700

    Tests a single insertion

--- test/PgQuerySpec.hs
+++ test/PgQuerySpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module PgQuerySpec where
 
 import           Test.Hspec
@@ -5,23 +7,33 @@ import           Test.Hspec
 import           Database.HDBC
 import           Database.HDBC.PostgreSQL
 
-loadFixture :: String -> IO ()
+import           PgQuery                  (insert)
+import           Types                    (SqlRow (..))
+
+loadFixture :: String -> IO Connection
 loadFixture name = do
   sql <- readFile $ "test/fixtures/" ++ name ++ ".sql"
-  conn <- connectPostgreSQL "postgres://postgres:@localhost:5432/dbapi_test"
+  conn <- connectPostgreSQL "postgres://localhost:5432/dbapi_test"
+  runRaw conn "drop schema if exists public cascade"
+  runRaw conn "create schema public"
   runRaw conn sql
   commit conn
+  return conn
 
 main :: IO ()
-main = do
-  hspec spec
+main = hspec spec
 
 spec :: Spec
 spec = beforeAll (loadFixture "schema") $ do
   describe "insert" $
-    it "can insert into an empty table" $
-      head [23 ..] `shouldBe` (23 :: Int)
+    it "can insert into an empty table" $ \conn -> do
+      _ <- insert "auto_incrementing_pk" (SqlRow [
+          ("non_nullable_string", toSql ("a string that isn't null" :: String))
+        ]) conn
+      r <- quickQuery conn "select count(1) from auto_incrementing_pk" []
+      commit conn
+      [[toSql (1 :: Int)]] `shouldBe` r
 
   describe "insert again" $
-    it "is true" $
+    it "is true" $ \_ ->
       True `shouldBe` True
